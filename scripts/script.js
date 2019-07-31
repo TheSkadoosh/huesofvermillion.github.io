4b6d1ef1a06f12e6e04d63df35f37438352b5d50
Leigh <HuesofVermillion@users.noreply.github.com>
2019-07-30 19:51:42 -0600

    Updated H1 to change with swap of color vs img

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,17 @@
 // This is defining the colors in the random array
-var colorRand = [ 'black', 'white', 'orange', 'blue'];
+var colorRand = [ 'black', 'green', 'orange', 'blue'];
 // Function used to change the background to the colorRand array of colors for the background
 function backgroundColor() {
+    document.getElementById("header1").style.color = "#780a02";
     document.getElementById("bodyChange").style.backgroundImage = "none";
     document.getElementById("bodyChange").style.backgroundColor = colorRand[Math.floor(Math.random() * colorRand.length)];
 }
 
 // This is defining the images to use in the random array
-// var backgroundImg = new Array();
-// backgroundImg = 'red0.png';
-// backgroundImg = 'red1.jpg';
-// backgroundImg = 'red2.jpg';
-var bgImg = [ "url('red0.png')", "url('red1.jpg')", "url('red2.jpg')"];
+var bgImg = [ "url('images/red0.png')", "url('images/red1.jpg')", "url('images/red2.jpg')"];
 // Function used to change the background images to the backgroundImg array of images
 function backgroundImg() {
     document.getElementById("bodyChange").style.backgroundImage = bgImg[Math.floor(Math.random() * bgImg.length)];
+    document.getElementById("header1").style.color = "white";
 }
 1
